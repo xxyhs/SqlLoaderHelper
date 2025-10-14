6799bc6093a644f06ddbd699c99c1c6088be2a2d
xiongkailing <xiongkailing@gmail.com>
2025-10-14 10:31:50 +0800

    lint code

--- SQLLoaderHelper.cs
+++ SQLLoaderHelper.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Task = System.Threading.Tasks.Task;
+using System.Threading.Tasks;
 
 namespace SqlLoaderHelper
 {
